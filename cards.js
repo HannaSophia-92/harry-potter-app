614096067fd96f587ae0086ac730e9d87bf00bbd
Merle Makoben <merlemakoben@gmail.com>
2022-02-25 16:56:08 +0100

    Edit toggle card

--- cards.js
+++ cards.js
@@ -24,10 +24,18 @@ function cards() {
 
         cardElement.innerHTML = `
         <button data-js="listButton" class="list__button"> 
+        <div data-js="character" class="list__character">
         <h2 class="list__characterName">${character.name}</h2>
-        <img class="list__card-image" src="${character.image}"/> 
-        <div>
-        <p data-js="character-infos" hidden>Name: ${character.name}</p>
+        <img data-js="character__image" class="list__card-image" src="${character.image}"/> 
+        </div>
+        <div data-js="character-infos" hidden>
+        <ul class=> 
+        <li> Date of Birth: ${character.dateOfBirth}</li>
+        <li> House: ${character.house}</li>
+        <li> Wizard: ${character.wizard}</li>
+        <li> Patronus: ${character.patronus}</li>
+        <li> Actor: ${character.actor}</li>
+        </ul>
         </div>
         </button>
         
@@ -36,9 +44,17 @@ function cards() {
 
         const characterInfo = cardElement.querySelector('[data-js="character-infos"]')
         const listButton = cardElement.querySelector('[data-js="listButton"]')
+        const characterHeader = cardElement.querySelector('[data-js="character"]')
+        const characterImage = cardElement.querySelector('[data-js="character__image"]')
 
         listButton.addEventListener('click', () => {
+            listButton.classList.toggle('card__element--active')
+            characterHeader.classList.toggle('list__character--active')
+            characterImage.classList.toggle('list__card-image--active')
+            characterImage.classList.toggle('list__card-image')
             characterInfo.toggleAttribute('hidden')
+
+            
         })
     })
     }
