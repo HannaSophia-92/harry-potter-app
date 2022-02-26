e1501904853970c6b537279c26e41349ac986fc4
Hanna Sophia Schlenker <hanna-schlenker@web.de>
2022-02-26 14:12:35 +0100

    Add styles and footer

--- cards.js
+++ cards.js
@@ -1,35 +1,36 @@
-
 function cards() {
-    fetchPeople();
-    async function fetchPeople(){
-        try{
-            const response = await fetch('http://hp-api.herokuapp.com/api/characters');
-            const data = await response.json();
-            console.log(data);
-            createCharacterList(data);
-        }
-        catch(error) { console.log(error)
-        }
+  fetchPeople();
+  async function fetchPeople() {
+    try {
+      const response = await fetch(
+        'http://hp-api.herokuapp.com/api/characters'
+      );
+      const data = await response.json();
+      console.log(data);
+      createCharacterList(data);
+    } catch (error) {
+      console.log(error);
     }
+  }
 
-    function createCharacterList(characters) {
-        const main = document.querySelector('[data-js="main"]')
-        const listElement = document.createElement('ul');
-        listElement.className = 'cards__container-list';
-        main.append(listElement);
-    
+  function createCharacterList(characters) {
+    const main = document.querySelector('[data-js="main"]');
+    const listElement = document.createElement('ul');
+    listElement.className = 'cards__container-list';
+    main.append(listElement);
+
     characters.forEach(character => {
-        const cardElement = document.createElement('li');
-        cardElement.className = 'card__element';
+      const cardElement = document.createElement('li');
+      cardElement.className = 'card__element';
 
-        cardElement.innerHTML = `
+      cardElement.innerHTML = `
         <button data-js="listButton" class="cards__container-button"> 
          <div data-js="character">
             <img data-js="character__image" class="cards__container-image" aria-label="picture of" src="${character.image}"/> 
             <h2 class="cards__characterName">${character.name}</h2>
          </div>
-        <div data-js="character-infos" class="cards__character-infos" hidden>
-        <ul> 
+        <div data-js="character-infos" class="cards__character-infos-container" hidden>
+        <ul class="cards__character-infos"> 
             <li> Date of Birth: ${character.dateOfBirth}</li>
             <li> House: ${character.house}</li>
             <li> Wizard: ${character.wizard}</li>
@@ -39,29 +40,29 @@ function cards() {
         </div>
         </button>
         
-        `
-        listElement.append(cardElement);
+        `;
+      listElement.append(cardElement);
 
-        const characterInfo = cardElement.querySelector('[data-js="character-infos"]')
-        const listButton = cardElement.querySelector('[data-js="listButton"]')
-        const characterHeader = cardElement.querySelector('[data-js="character"]')
-        const characterImage = cardElement.querySelector('[data-js="character__image"]')
+      const characterInfo = cardElement.querySelector(
+        '[data-js="character-infos"]'
+      );
+      const listButton = cardElement.querySelector('[data-js="listButton"]');
+      const characterHeader = cardElement.querySelector(
+        '[data-js="character"]'
+      );
+      const characterImage = cardElement.querySelector(
+        '[data-js="character__image"]'
+      );
 
-        listButton.addEventListener('click', () => {
-            listButton.classList.toggle('card__element--active')
-            characterHeader.classList.toggle('cards__character--active')
-            characterImage.classList.toggle('cards__image--active')
-            characterImage.classList.toggle('cards__container-image')
-            characterInfo.toggleAttribute('hidden')
-
-            
-        })
-    })
-    }
+      listButton.addEventListener('click', () => {
+        listButton.classList.toggle('card__element--active');
+        characterHeader.classList.toggle('cards__character--active');
+        characterImage.classList.toggle('cards__image--active');
+        characterImage.classList.toggle('cards__container-image');
+        characterInfo.toggleAttribute('hidden');
+      });
+    });
+  }
 }
 
-
-
-
-
-export default cards
+export default cards;
